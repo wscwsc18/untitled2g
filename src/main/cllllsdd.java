3c81374c87ad9254ac0b4da16db92cf5fd937115
行行 <454444412@qq.com>
2019-10-23 14:56:38 +0800

    提交 12

---
+++ src/main/cllllsdd.java
@@ -0,0 +1,17 @@
+package main;
+
+public class cllllsdd {
+    private String dkdkdklddlk;
+
+    public String getDkdkdklddlk() {
+        return dkdkdklddlk;
+    }
+
+    public void setDkdkdklddlk(String dkdkdklddlk) {
+        this.dkdkdklddlk = dkdkdklddlk;
+    }
+
+
+    //????
+}
+
