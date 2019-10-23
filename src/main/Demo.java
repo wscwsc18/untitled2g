145c53e2a9a9ec2c8ebd9312141c1a58569c6685
行行 <454444412@qq.com>
2019-10-23 14:36:39 +0800

    第一次 提交  10.22.2点

---
+++ src/main/Demo.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Demo {
+
+    Integer index;
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+}
