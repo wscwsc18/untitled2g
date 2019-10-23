437a39825142dabf05a42b047b282a4fce73a6f8
行行 <454444412@qq.com>
2019-10-23 14:50:21 +0800

    大家都决定将积极解决经济地地道道的  提交 11

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 
     Integer index;
 
+    Integer index1;
+
     public Integer getIndex() {
         return index;
     }
@@ -29,15 +31,24 @@ public class Demo {
         this.index = index;
     }
 
+    public Integer getIndex1() {
+        return index1;
+    }
+
+    public void setIndex1(Integer index1) {
+        this.index1 = index1;
+    }
+
 /*
 sadkj四度空间的看法 得到的
     \
     \
     zxvzxcxcz
-*/
 
+    拾掇拾掇
+真实的v
 
-
+ */
 
 
 }
